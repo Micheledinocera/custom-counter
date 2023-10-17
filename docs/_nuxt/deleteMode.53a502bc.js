7783248a58a4e2131a293d345020679bf25feece
MDinocera <micheledinocera@gmail.com>
2023-10-17 12:28:44 +0200

    next deploy

--- docs/_nuxt/deleteMode.53a502bc.js
+++
@@ -1,1 +0,0 @@
-import{H as t,X as o}from"./entry.909f2f5c.js";const l=()=>o("deleteMode",()=>!1),c=()=>{const e=l(),s=t();return{disableDeleteMode:()=>{e&&(e.value=!1,s.value=[])}}};export{c as a,l as u};
