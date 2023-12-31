58417bbd97abbde64c52f9562080d6b6122263e3
MDinocera <micheledinocera@gmail.com>
2023-12-31 11:02:43 +0100

    next build

--- docs/_nuxt/deleteMode.9fc795fc.js
+++
@@ -1,1 +0,0 @@
-import{H as t,Y as o}from"./entry.1762740f.js";const l=()=>o("deleteMode",()=>!1),c=()=>{const e=l(),s=t();return{disableDeleteMode:()=>{e&&(e.value=!1,s.value=[])}}};export{c as a,l as u};
---
+++ docs/_nuxt/deleteMode.c2aa8ec8.js
@@ -0,0 +1,1 @@
+import{H as t,Z as o}from"./entry.d87b3aa5.js";const l=()=>o("deleteMode",()=>!1),c=()=>{const e=l(),s=t();return{disableDeleteMode:()=>{e&&(e.value=!1,s.value=[])}}};export{c as a,l as u};
