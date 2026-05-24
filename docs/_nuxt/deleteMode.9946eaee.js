14f29100357388b499113cbd284d46283d96de97
Michele Di Nocera <mdinocera.digital@gmail.com>
2026-05-24 15:21:41 +0200

    new deploy

--- docs/_nuxt/deleteMode.9946eaee.js
+++
@@ -1,1 +0,0 @@
-import{H as t,a2 as o}from"./entry.86a1c0ae.js";const l=()=>o("deleteMode",()=>!1),c=()=>{const e=l(),s=t();return{disableDeleteMode:()=>{e&&(e.value=!1,s.value=[])}}};export{c as a,l as u};
